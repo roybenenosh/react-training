23244d484d500eef227edb07e12ef7982956065b
rbenenosh <roybenenosh@ebay.com>
2020-11-04 00:07:14 +0200

    Fix PR

--- lab27/exercise2/src/starWarsDisplay.js
+++ lab27/exercise2/src/starWarsDisplay.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import $ from 'jquery';
+import $ from 'jquery/dist/jquery';
 
 export default function({ id }) {
   const [movieDetails, setMovieDetails] = useState({});
